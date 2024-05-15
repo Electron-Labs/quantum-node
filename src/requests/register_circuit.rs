6cdf792ff398c3248c2176c8b6f7234dce1c0da8
electron-team <utsavjn48@gmail.com>
2024-05-15 15:13:52 +0530

    feat: vkey keccak256 added
    Signed-off-by: electron-team <utsavjn48@gmail.com>

--- src/requests/register_circuit.rs
+++ src/requests/register_circuit.rs
@@ -1,11 +1,16 @@
+use keccak_hash::{keccak};
+
 use crate::types::{register_circuit::RegisterCircuitRequest, register_circuit::RegisterCircuitResponse};
 
 pub fn register_circuit(data: RegisterCircuitRequest) -> RegisterCircuitResponse {
     // 1. Dump whatever you need in db/queue 
     // 2. Just return the keccak hash of the data.vkey at this point
+    // Keccak hash borshified vkey for now
+    let vkey = data.vkey.clone();
+    let mut keccak_ip = vkey.as_slice();
+    let hash = keccak(&mut keccak_ip);
     // 3. An async worker actually takes care of the registration request
-    let hash = "0x00";
     RegisterCircuitResponse { 
-        circuit_hash: String::from(hash)
+        circuit_hash: hash.to_string()
     }
 }
