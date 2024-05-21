120e97d8df285ad1affae327ad5b5e5ffddee39d
electron-team <utsavjn48@gmail.com>
2024-05-21 18:21:34 +0530

    feat: prover registration flow with few shallow functions
    Signed-off-by: electron-team <utsavjn48@gmail.com>

--- quantum_types/src/types/db/task.rs
+++ quantum_types/src/types/db/task.rs
@@ -5,7 +5,7 @@ use crate::enums::task_type::TaskType;
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Task {
     pub id: Option<u64>,
-    pub user_circuit_hash: Option<String>,
+    pub user_circuit_hash: String,
     pub task_type: TaskType,
     pub proof_id: Option<u64>,
     pub task_status: TaskType
---
+++ quantum_worker/src/registration.rs
@@ -0,0 +1,72 @@
+use std::{fmt::Error, fs};
+
+use quantum_db::repository::{reduction_circuit_repository::add_reduction_circuit_row, user_circuit_data_repository::{get_user_circuit_data_by_circuit_hash, update_user_circuit_data_redn_circuit, update_user_circuit_data_reduction_status}};
+use quantum_types::{enums::{circuit_reduction_status::CircuitReductionStatus, proving_schemes::ProvingSchemes, task_type::TaskType}, types::db::{reduction_circuit::{self, ReductionCircuit}, task::Task}};
+use anyhow::{Ok, Result as AnyhowResult};
+use sqlx::{MySql, Pool};
+use quantum_reduction_circuits_ffi::circuit_builder::{BuildResult, CircomVK, GnarkVK};
+
+pub async fn handle_registration_task(pool: &Pool<MySql>, registration_task: Task) -> AnyhowResult<()> {
+    assert_eq!(registration_task.task_type, TaskType::CircuitReduction);
+    let user_circuit_hash = registration_task.user_circuit_hash;
+    
+    // get user_circuit_data
+    let user_circuit_data = get_user_circuit_data_by_circuit_hash(pool, &user_circuit_hash).await?;
+    
+    // Get vk_path
+    let user_vk_path = user_circuit_data.vk_path;
+    
+    // Load User Vkey
+    let user_vk_data = fs::read_to_string(user_vk_path)?;
+    
+    let build_result: BuildResult;
+
+    // Call build_reduction_circuit from quantum_reduction_circuit
+    if user_circuit_data.proving_scheme == ProvingSchemes::GnarkGroth16 {
+        let gnark_vkey: GnarkVK = serde_json::from_str(&user_vk_data)?;
+        build_result = gnark_vkey.build(user_circuit_data.pis_len as u8);
+    } else if user_circuit_data.proving_scheme == ProvingSchemes::Groth16 {
+        let snarkjs_vkey: CircomVK = serde_json::from_str(&user_vk_data)?;
+        build_result = snarkjs_vkey.build();
+    } else {
+        return Ok(());
+    }
+
+    // Check if build was done successfully
+    if !build_result.pass{
+        return Err(anyhow::Error::msg(build_result.msg));
+    }
+    // Dump reduction circuit proving key and verification key as raw bytes 
+    // TODO: Utkarsh
+    let pk_path = "";
+    let vk_path = "";
+
+    // Add reduction circuit row (pk_path, vk_path, pis_len)
+    let reduction_circuit = ReductionCircuit {
+        id: None,
+        proving_key_path: String::from(pk_path),
+        vk_path: String::from(vk_path),
+        pis_len: user_circuit_data.pis_len,
+    };
+    let reduction_circuit_id = add_reduction_circuit_row(pool, reduction_circuit).await?;
+
+    // Add reduction circuit id to user_circuit_data
+    update_user_circuit_data_redn_circuit(pool, &user_circuit_hash, reduction_circuit_id).await?;
+
+    Ok(())
+}
+
+
+// #[cfg(test)]
+// mod tests {
+//     use std::fs;
+
+//     use quantum_reduction_circuits_ffi::circuit_builder::GnarkVK;
+
+//     #[test]
+//     pub fn test_ffi() {
+//         let json_data = fs::read_to_string("/Users/utsavjain/Desktop/electron_labs/quantum/quantum-node/dumps/gnark_vkey.json").expect("Failed to read file");
+// 		let gnark_vkey: GnarkVK = serde_json::from_str(&json_data).expect("Failed to deserialize JSON data");
+//         let x = gnark_vkey.build(1);
+//     }
+// }
