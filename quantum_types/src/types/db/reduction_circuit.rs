9a4dc1ca48f28af6d5c8c5a21a2619150840b5fd
electron-team <utsavjn48@gmail.com>
2024-05-23 11:45:16 +0530

    feat: change circuit id type
    Signed-off-by: electron-team <utsavjn48@gmail.com>

--- quantum_types/src/types/db/reduction_circuit.rs
+++ quantum_types/src/types/db/reduction_circuit.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct ReductionCircuit {
-    pub id: Option<u64>,
+    pub circuit_id: String,
     pub proving_key_path: String,
     pub vk_path: String,
     pub pis_len: u8
