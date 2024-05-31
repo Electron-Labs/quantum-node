cce054a035493676123ae273688d5ecb3eb6a016
electron-team <utsavjn48@gmail.com>
2024-05-31 17:40:03 +0530

    fix: mark reduction complete if ckt already registered
    Signed-off-by: electron-team <utsavjn48@gmail.com>

--- quantum_api_server/src/service/register_circuit.rs
+++ quantum_api_server/src/service/register_circuit.rs
@@ -39,7 +39,7 @@ pub async fn register_circuit_exec<T: Vkey>(data: RegisterCircuitRequest, config
     let reduction_circuit_id = handle_reduce_circuit(data.num_public_inputs, data.proof_type).await?;
 
     // Add user circuit data to DB
-    insert_user_circuit_data(get_pool().await, &circuit_hash_string, &vkey_path, reduction_circuit_id.clone(), data.num_public_inputs, data.proof_type,CircuitReductionStatus::NotPicked, &protocol.protocol_name).await?;
+    insert_user_circuit_data(get_pool().await, &circuit_hash_string, &vkey_path, reduction_circuit_id.clone(), data.num_public_inputs, data.proof_type,if is_circuit_already_registered {CircuitReductionStatus::Completed} else {CircuitReductionStatus::NotPicked}, &protocol.protocol_name).await?;
 
     // Create a reduction task for Async worker to pick up later on
     create_circuit_reduction_task(reduction_circuit_id, &circuit_hash_string).await?;
