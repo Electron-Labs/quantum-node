4a3f6ffcbae9473a1f0239f3fc9fcee8f08550cf
utsavjnn <utsavjn48@gmail.com>
2024-06-06 13:09:25 +0000

    fix: decode hex without 0x
    Signed-off-by: utsavjnn <utsavjn48@gmail.com>

--- quantum_utils/src/keccak.rs
+++ quantum_utils/src/keccak.rs
@@ -17,7 +17,7 @@ pub fn encode_keccak_hash(keccak_bytes: &[u8; 32]) -> AnyhowResult<String> {
 
 pub fn decode_keccak_hex(keccak_hex: &str) -> AnyhowResult<[u8; 32]> {
     let mut decoded  = [0; 32];
-    hex::decode_to_slice(keccak_hex, &mut decoded)?;
+    hex::decode_to_slice(&keccak_hex[2..], &mut decoded)?;
     Ok(decoded)
 }
 
