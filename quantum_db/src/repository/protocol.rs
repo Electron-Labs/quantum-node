eeb011cbaf1dbfb20a487840ddeb5553bc141f57
aditya2499 <adbansal99@gmail.com>
2024-07-26 16:39:45 +0530

    feat: support duplicate proof for selected protocol

--- quantum_db/src/repository/protocol.rs
+++ quantum_db/src/repository/protocol.rs
@@ -2,7 +2,8 @@ use quantum_types::{types::db::protocol::Protocol};
 use quantum_utils::error_line;
 use sqlx::{mysql::MySqlRow, Execute, MySql, Pool, Row};
 use tracing::info;
-use anyhow::{anyhow, Result as AnyhowResult};
+use anyhow::{anyhow, Error, Result as AnyhowResult};
+use crate::error::error::CustomError;
 
 pub async fn get_protocol_by_auth_token(pool: &Pool<MySql>, auth_token: &str) -> AnyhowResult<Option<Protocol>> {
      // oldest_entry(task_status: TaskStatus::NotPicked)
@@ -19,12 +20,28 @@ pub async fn get_protocol_by_auth_token(pool: &Pool<MySql>, auth_token: &str) ->
     Ok(protocol)
 }
 
+pub async fn get_protocol_by_protocol_name(pool: &Pool<MySql>, protocol_name: &str) -> AnyhowResult<Protocol> {
+    // oldest_entry(task_status: TaskStatus::NotPicked)
+    let query  = sqlx::query("SELECT * from protocol where protocol_name = ?")
+        .bind(protocol_name);
+
+    info!("{}", query.sql());
+    info!("arguments: {}", protocol_name);
+
+    let protocol = match query.fetch_one(pool).await.map_err(|err| anyhow!(error_line!(err))) {
+        Ok(p) => get_protocol_from_row(p),
+        Err(e) => Err(anyhow!(CustomError::DB(error_line!(e))))
+    };
+    protocol
+}
+
 pub fn get_protocol_from_row(row: MySqlRow) -> AnyhowResult<Protocol>{
     println!("{:?}", row);
     Ok( 
         Protocol {
             protocol_name: row.try_get_unchecked("protocol_name").map_err(|err| anyhow!(error_line!(err)))?,
             auth_token: row.try_get_unchecked("auth_token").map_err(|err| anyhow!(error_line!(err)))?,
+            is_proof_repeat_allowed: row.try_get_unchecked("is_proof_repeat_allowed").map_err(|err| anyhow!(error_line!(err)))?,
         }
     )
 }
