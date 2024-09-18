97af1dd14e4a47db483c9a13c19c9cc8f5fea119
aditya2499 <adbansal99@gmail.com>
2024-09-18 11:16:04 +0000

    feat: agg_proof conversion and dumping logic

--- quantum_utils/src/paths.rs
+++ quantum_utils/src/paths.rs
@@ -54,6 +54,9 @@ pub fn get_reduction_circuit_pis_path(storage_folder_path: &str, reduced_pis_pat
     let pis_path = format!("{}/reduced_pis_{}.bin", reduced_pis_dir, proof_hash);
     pis_path
 }
+pub fn get_superproof_proof_path(storage_folder_path: &str, superproof_path: &str, superproof_id: u64) -> String {
+    format!("{}{}/{}/proof.bin", storage_folder_path, superproof_path, superproof_id)
+}
 
 pub fn get_superproof_proof_receipt_path(storage_folder_path: &str, superproof_path: &str, superproof_id: u64) -> String {
     format!("{}{}/{}/receipt.bin", storage_folder_path, superproof_path, superproof_id)
--- quantum_worker/src/aggregator.rs
+++ quantum_worker/src/aggregator.rs
@@ -7,21 +7,18 @@ use num_bigint::BigUint;
 use quantum_circuits_interface::ffi::circuit_builder::{CircomProof, CircomVKey, CircuitBuilder, CircuitBuilderImpl, GnarkVKey, ProveResult, Risc0SnarkProveArgs};
 use quantum_db::repository::{
     bonsai_image::get_aggregate_circuit_bonsai_image, superproof_repository::{
-        get_last_verified_superproof, update_previous_superproof_root, update_superproof_agg_time, update_superproof_leaves_path, update_superproof_proof_path, update_superproof_root, update_superproof_total_proving_time
+        get_last_verified_superproof, update_previous_superproof_root, update_superproof_agg_time, update_superproof_leaves_path, update_superproof_pis_path, update_superproof_proof_path, update_superproof_receipts_path, update_superproof_root, update_superproof_total_proving_time
     }, user_circuit_data_repository::get_user_circuit_data_by_circuit_hash
 };
 use quantum_types::{
     enums::proving_schemes::ProvingSchemes,
-    traits::{pis::Pis, vkey::Vkey},
+    traits::{pis::Pis, proof::Proof, vkey::Vkey},
     types::{
-        config::ConfigData,
-        db::proof::Proof as DBProof,
-        halo2_plonk::{Halo2PlonkPis, Halo2PlonkVkey},
-        snarkjs_groth16::{SnarkJSGroth16Pis, SnarkJSGroth16Vkey},
+        config::ConfigData, db::proof::Proof as DBProof, gnark_groth16::{GnarkGroth16Pis, GnarkGroth16Proof}, halo2_plonk::{Halo2PlonkPis, Halo2PlonkVkey}, snarkjs_groth16::{SnarkJSGroth16Pis, SnarkJSGroth16Vkey}
     },
 };
 use quantum_utils::{
-    error_line, file::{dump_object, read_bytes_from_file, read_file, write_bytes_to_file}, keccak::encode_keccak_hash, paths::{get_cs_bytes_path, get_inner_vkey_path, get_snark_reduction_pk_bytes_path, get_snark_reduction_vk_path, get_superproof_leaves_path, get_superproof_proof_receipt_path, get_superproof_snark_receipt_path, get_user_vk_path}
+    error_line, file::{dump_object, read_bytes_from_file, read_file, write_bytes_to_file}, keccak::encode_keccak_hash, paths::{get_cs_bytes_path, get_inner_vkey_path, get_snark_reduction_pk_bytes_path, get_snark_reduction_vk_path, get_superproof_leaves_path, get_superproof_pis_path, get_superproof_proof_path, get_superproof_proof_receipt_path, get_superproof_snark_receipt_path, get_user_vk_path}
 };
 use risc0_zkvm::{serde::to_vec, Receipt};
 use serde::Serialize;
@@ -36,6 +33,7 @@ pub async fn handle_proof_aggregation_and_updation(
     config: &ConfigData,
 ) -> AnyhowResult<()> {
 
+    // TODO: add all object inside a single object
     let (receipt, snark_receipt, aggregation_result, aggregation_time) = handle_proof_aggregation(proofs.clone(), superproof_id, config).await?;
     info!("aggregation done in time : {:?}", aggregation_time);
 
@@ -43,26 +41,39 @@ pub async fn handle_proof_aggregation_and_updation(
         return Err(anyhow::Error::msg(error_line!(aggregation_result.msg)));
     }
     // TODO: Dump superproof receipt and add to the DB
-    let superproof_proof_path = get_superproof_proof_receipt_path(
+    let superproof_receipt_path = get_superproof_proof_receipt_path(
         &config.storage_folder_path,
         &config.supperproof_path,
         superproof_id,
     );
-    dump_object(receipt.unwrap(), &superproof_proof_path)?;
+    dump_object(receipt.unwrap(), &superproof_receipt_path)?;
 
-    let superproof_proof_path = get_superproof_snark_receipt_path(
+    let superproof_snark_receipt_path = get_superproof_snark_receipt_path(
         &config.storage_folder_path,
         &config.supperproof_path,
         superproof_id,
     );
-    dump_object(snark_receipt, &superproof_proof_path)?;
+    dump_object(snark_receipt, &superproof_snark_receipt_path)?;
     // superproof_proof.dump_proof(&superproof_proof_path)?;
+
+    let superproof_proof = GnarkGroth16Proof::from_risc0_gnark_proof_result(aggregation_result.proof);
+    let superproof_pis = GnarkGroth16Pis(aggregation_result.pub_inputs);
+
+    let superproof_proof_path = get_superproof_proof_path(&config.storage_folder_path, &config.supperproof_path, superproof_id);
+    let superproof_pis_path = get_superproof_pis_path(&config.storage_folder_path, &config.supperproof_path, superproof_id);
+
+    superproof_proof.dump_proof(&superproof_proof_path)?;
+    superproof_pis.dump_pis(&superproof_pis_path)?;
 
     // TODO: Add new field superproof receipt path
     update_superproof_proof_path(get_pool().await, &superproof_proof_path, superproof_id).await?;
 
+    update_superproof_pis_path(get_pool().await, &superproof_pis_path, superproof_id).await?;
     // Add agg_time to the db
     update_superproof_agg_time(get_pool().await, aggregation_time.as_secs(), superproof_id).await?;
+
+    update_superproof_receipts_path(get_pool().await, &superproof_receipt_path, &superproof_snark_receipt_path, superproof_id).await?;
+    
 
     let proof_with_max_reduction_time = proofs.iter().max_by_key(|proof| proof.reduction_time);
     // TODO: remove unwrap , check reduction time is not getting update in db
@@ -163,6 +174,7 @@ fn snark_to_gnark_reduction(snark_receipt: &SnarkReceipt, config: &ConfigData, c
     
     let inner_proof = form_circom_proof_from_snark_receipt(snark_receipt);
 
+    println!("inner_proof: {:?}", inner_proof);
     let inner_vkey_path = get_inner_vkey_path(&config.storage_folder_path, &config.snark_reduction_data_path);
     let inner_v_key: CircomVKey = read_file(&inner_vkey_path)?;
 
@@ -174,8 +186,10 @@ fn snark_to_gnark_reduction(snark_receipt: &SnarkReceipt, config: &ConfigData, c
 
     let mut circuit_verifying_id_bytes = vec![];
     for i in circuit_verifying_id {
-        circuit_verifying_id_bytes.extend_from_slice(&i.to_be_bytes());
+        circuit_verifying_id_bytes.extend_from_slice(&i.to_le_bytes());
     }
+
+    println!("circuit_verifying_id_bytes: {:?}", circuit_verifying_id_bytes);
     let args = Risc0SnarkProveArgs {
         cs_bytes,
         pk_bytes,
@@ -204,9 +218,9 @@ fn form_circom_proof_from_snark_receipt(snark_receipt: &SnarkReceipt) -> CircomP
     let c0 = BigUint::from_bytes_be(&snark_receipt.snark.c[0]).to_string();
 
     CircomProof {
-        A: vec![a0,a1],
-        B: vec![vec![b00, b01], vec![b10, b11]],
-        C: vec![c0,c1],
+        A: vec![a0,a1,"1".to_string()],
+        B: vec![vec![b00, b01], vec![b10, b11], vec!["1".to_string(), "0".to_string()]],
+        C: vec![c0,c1,"1".to_string()],
         Protocol: "groth16".to_string(),
         Curve: "bn128".to_string(),
     }
@@ -219,24 +233,48 @@ fn form_bonsai_input_data<H: Hasher + Serialize>(agg_input: AggInputs<H>) -> Any
     Ok(input_data_vec)
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use dotenv::dotenv;
-//     use quantum_db::repository::proof_repository::get_proofs_in_superproof_id;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use dotenv::dotenv;
+    use quantum_db::repository::proof_repository::get_proofs_in_superproof_id;
+    use risc0_zkvm::Groth16Seal;
+    // use risc0_zkvm::Groth16Seal;
 
-//     #[tokio::test]
-//     #[ignore]
-//     pub async fn test_aggregate_proof_by_superproof_id() {
-//         // NOTE: it connect to database mentioned in the env file, to connect to the test db use .env.test file
-//         // dotenv::from_filename("../.env.test").ok();
-//         // dotenv().ok();
-//         let config_data = ConfigData::new("../../config.yaml"); // change the path
-//         let superproof_id = 90; // insert your circuit hash
-//         let superproof = get_superproof_by_id(get_pool().await, superproof_id).await.unwrap();
-//         let proofs = get_proofs_in_superproof_id(get_pool().await,superproof_id).await.unwrap();
-//         let (result, reduction_time) = handle_proof_aggregation(proofs, superproof_id, &config_data).await.unwrap();
-//         println!("{:?}", result);
-//         assert_eq!(result.success, true);
-//     }
-// }
+    // #[tokio::test]
+    // #[ignore]
+    // pub async fn test_aggregate_proof_by_superproof_id() {
+    //     // NOTE: it connect to database mentioned in the env file, to connect to the test db use .env.test file
+    //     // dotenv::from_filename("../.env.test").ok();
+    //     // dotenv().ok();
+    //     let config_data = ConfigData::new("../../config.yaml"); // change the path
+    //     let superproof_id = 90; // insert your circuit hash
+    //     let superproof = get_superproof_by_id(get_pool().await, superproof_id).await.unwrap();
+    //     let proofs = get_proofs_in_superproof_id(get_pool().await,superproof_id).await.unwrap();
+    //     let (result, reduction_time) = handle_proof_aggregation(proofs, superproof_id, &config_data).await.unwrap();
+    //     println!("{:?}", result);
+    //     assert_eq!(result.success, true);
+    // }
+
+    #[tokio::test]
+    #[ignore]
+    pub async fn test_form_circom_proof_from_snark_receipt() {
+        let s = SnarkReceipt {
+            snark: Groth16Seal { 
+                a: [[7, 134, 10, 194, 220, 228, 10, 147, 40, 206, 193, 121, 215, 185, 233, 37, 12, 210, 197, 63, 197, 65, 179, 2, 91, 139, 217, 89, 45, 49, 216, 29].to_vec(), [17, 91, 2, 136, 175, 111, 16, 178, 68, 251, 57, 84, 14, 106, 249, 16, 70, 141, 148, 178, 98, 231, 105, 18, 3, 70, 155, 94, 117, 116, 145, 150].to_vec()].to_vec()
+                
+                , b: [[[0, 228, 185, 81, 10, 211, 129, 33, 230, 143, 33, 138, 100, 223, 124, 208, 91, 54, 72, 154, 78, 241, 253, 229, 79, 199, 121, 123, 131, 100, 130, 125].to_vec(), [18, 97, 110, 247, 50, 52, 74, 11, 230, 212, 47, 180, 154, 109, 146, 178, 184, 153, 159, 140, 84, 172, 18, 4, 31, 8, 213, 11, 220, 232, 153, 107].to_vec()].to_vec(), [[19, 166, 95, 171, 8, 120, 66, 22, 239, 59, 122, 217, 8, 65, 243, 108, 103, 223, 183, 243, 183, 237, 6, 223, 51, 179, 216, 39, 146, 84, 8, 25].to_vec(), [15, 17, 171, 251, 180, 143, 74, 130, 103, 137, 117, 45, 217, 193, 67, 134, 6, 181, 245, 87, 196, 106, 14, 248, 47, 118, 207, 148, 109, 12, 37, 87].to_vec()].to_vec()].to_vec()
+                
+                , c:[[17, 175, 26, 146, 194, 183, 124, 180, 212, 2, 35, 15, 247, 235, 250, 183, 162, 197, 198, 39, 76, 1, 121, 3, 107, 105, 141, 31, 134, 76, 72, 248].to_vec(), [20, 11, 194, 53, 122, 101, 218, 241, 130, 184, 225, 18, 220, 224, 69, 91, 181, 95, 47, 123, 173, 16, 241, 244, 61, 136, 245, 165, 236, 160, 177, 194].to_vec()].to_vec()
+            
+            },
+
+
+            post_state_digest:  [163, 172, 194, 113, 23, 65, 137, 150, 52, 11, 132, 229, 169, 15, 62, 244, 196, 157, 34, 199, 158, 68, 170, 216, 34, 236, 156, 49, 62, 30, 184, 226].to_vec(),
+            journal: [90, 18, 196, 95, 51, 152, 136, 218, 195, 64, 81, 98, 111, 194, 77, 226, 56, 208, 183, 55, 189, 132, 182, 44, 38, 183, 233, 171, 12, 192, 231, 112].to_vec() ,
+        };
+
+        let proof = form_circom_proof_from_snark_receipt(&s);
+        println!("circom_proof: {:?}", proof);
+    }
+}
