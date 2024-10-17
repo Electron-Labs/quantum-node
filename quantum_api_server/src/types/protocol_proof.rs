03db5fe9f92d59ab2ddf81e68bd19f1bb0c0e455
utkarsh-21st <utkarshdhing@gmail.com>
2024-10-17 12:34:42 +0000

    update get_protocol_proof api

--- quantum_api_server/src/types/protocol_proof.rs
+++ quantum_api_server/src/types/protocol_proof.rs
@@ -6,6 +6,4 @@ use serde::Serialize;
 pub struct ProtocolProofResponse {
     pub merkle_proof_position: u64,
     pub merkle_proof: Vec<String>,
-    pub leaf_next_value: String, // hex
-    pub leaf_next_index: String,
 }
